e39cf95fbb6f84a6c1f7fb8abe9cce20fca0b883
adamcfd <91072420+adamcfd@users.noreply.github.com>
2022-06-10 03:53:23 +0000

    format

--- gcd/gcd.c
+++ gcd/gcd.c
@@ -1,17 +1,19 @@
-#include<stdio.h>
-int main(){
-    int a,b;
+#include <stdio.h>
+int main()
+{
+    int a, b;
     printf("please enter a:");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("please enter b:");
-    scanf("%d",&b);
-    printf("%d %d\n",a,b);
-    printf("gcd = %d\n",gcd(a,b));
+    scanf("%d", &b);
+    printf("%d %d\n", a, b);
+    printf("gcd = %d\n", gcd(a, b));
 }
 
-int gcd(int a,int b){
-    int r = a%b ;
-    if(r == 0)
+int gcd(int a, int b)
+{
+    int r = a % b;
+    if (r == 0)
         return b;
-    return gcd(b,r);
+    return gcd(b, r);
 }

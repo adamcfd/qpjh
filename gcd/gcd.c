d20f7829055b0a29a609b78e2fd35812724ec1b9
adamcfd <91072420+adamcfd@users.noreply.github.com>
2022-06-10 03:47:34 +0000

    gcd

---
+++ gcd/gcd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("please enter a:");
+    scanf("%d",&a);
+    printf("please enter b:");
+    scanf("%d",&b);
+    printf("%d %d\n",a,b);
+    printf("gcd = %d\n",gcd(a,b));
+}
+
+int gcd(int a,int b){
+    int r = a%b ;
+    if(r == 0)
+        return b;
+    return gcd(b,r);
+}
